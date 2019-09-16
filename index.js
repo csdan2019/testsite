58e3694c481dd4c71baeaf7f54317028b0bd01f8
csdan2019 <49932259+csdan2019@users.noreply.github.com>
2019-09-16 00:15:28 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 2120);
+app.use(express.static('views/images')); 
+//app.use(express.static('views/css'));
+app.use(express.static('public'));
+
+app.get('/',function(req,res){
+  res.render('home') //We can omit the .handlebars extension as we do below
+});
+app.get('/contact', function(req, res) {
+    res.render('contact');
+});
+
+app.get('/background', function(req, res) {
+    res.render('background');
+});
+
+app.get('/resume', function(req, res) {
+	
+    res.render('resume');
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on ' + app.get('port') + '; press Ctrl-C to terminate.');
+});
---
+++ public/stylesheets/resume.js
@@ -0,0 +1,6 @@
+
+	var x=document.getElementsByClassName("school");
+x.style.backgroundColor="yellow";
+var y=document.getElementsbyTagName("div");
+y.style.backgroundColor="green";
+
